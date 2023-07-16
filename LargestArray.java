7ac8ee70f97969b2051c6f8ea2e7b64fe19428b0
unknown <suranjanbehura8@gmail.com>
2023-07-16 11:57:42 +0530

    BUG-22(smallest)

--- LargestArray.java
+++ LargestArray.java
@@ -10,12 +10,19 @@ public class LargestArray {
          }
          return largest;
     }
-    // public static int isSmallest(int n){
-
-    // }
+    public static int isSmallest(int number[]){
+         int smallest=Integer.MAX_VALUE;
+         for(int i=0;i<number.length;i++){
+            if(smallest>number[i]){
+                smallest=number[i];
+            }
+         }
+         return smallest;
+     }
        public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
          int number[]={12,56,4,23,1};
           System.out.println(isLargest(number));
+          System.out.println(isSmallest(number));
        }
 }
