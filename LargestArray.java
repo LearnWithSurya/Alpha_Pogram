a7320a69a40085c2288769bdaa00db60327ec0f8
unknown <suranjanbehura8@gmail.com>
2023-07-16 12:03:11 +0530

    BUG-34

--- LargestArray.java
+++ LargestArray.java
@@ -1,8 +1,9 @@
 import java.util.*;
 
 public class LargestArray {
+
     public static int isLargest(int number[]){
-        int largest=Integer.MIN_VALUE;
+        int largest=Integer.MIN_VALUE;//-infinity
          for(int i=0;i<number.length;i++){
             if(largest<number[i]){
                 largest=number[i];
@@ -11,7 +12,7 @@ public class LargestArray {
          return largest;
     }
     public static int isSmallest(int number[]){
-         int smallest=Integer.MAX_VALUE;
+         int smallest=Integer.MAX_VALUE;//+infinity
          for(int i=0;i<number.length;i++){
             if(smallest>number[i]){
                 smallest=number[i];
